385772102357c821db7f4450bd3cd12032f113e8
Nicolas Rod <nrq5510@gmail.com>
2017-12-28 23:24:14 -0500

    Added security to the api

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const axios = require('axios')
+const helmet = require('helmet')
+const RateLimit = require('express-rate-limit')
 const cheerio = require('cheerio')
 const Window = require('window')
 const window = new Window()
 
 const app = express()
+
+const limiter = new RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes 
+  max: 100, // limit each IP to 100 requests per windowMs 
+  delayMs: 0 // disable delaying - full speed until the max limit is reached 
+})
+
+app.use(limiter)
+app.use(helmet({
+  frameguard: {action: 'deny'}
+}))
 
 app.get('/', (req, res, next) => {
   axios.get('https://platzi.com/agenda/')
@@ -16,5 +29,5 @@ app.get('/', (req, res, next) => {
 })
 
 app.listen(3000, () => {
-  console.log('eeeee')
+  console.log('Server Runing')
 })
