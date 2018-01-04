262c922300bf7fa92f3684604cd7f46c83dc7766
Nicolas Rod <nrq5510@gmail.com>
2018-01-04 11:24:07 -0500

    Response modifier to send all in an array

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const axios = require('axios')
 const helmet = require('helmet')
 const RateLimit = require('express-rate-limit')
 const cheerio = require('cheerio')
+const fs = require('fs')
+const path = require('path')
 const Window = require('window')
 const window = new Window()
 
 const app = express()
 
 const limiter = new RateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes 
-  max: 100, // limit each IP to 100 requests per windowMs 
-  delayMs: 0 // disable delaying - full speed until the max limit is reached 
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  delayMs: 0 // disable delaying - full speed until the max limit is reached
 })
 
 app.use(limiter)
@@ -19,13 +21,40 @@ app.use(helmet({
   frameguard: {action: 'deny'}
 }))
 
-app.get('/', (req, res, next) => {
+app.get('/set-agenda-data/', (req, res, next) => {
   axios.get('https://platzi.com/agenda/')
     .then((response) => {
       var $ = cheerio.load(response.data)
       eval($('script')[17].children[0].data)
-      res.send(data.scheduleItems.agenda_all)
+      var dataArray = Object.values(data.scheduleItems.agenda_all.agenda_items)
+
+      dataArray.sort( (a, b) => {
+        return new Date(a.start_time) - new Date(b.start_time)
+      })
+      console.log('🤗')
+
+      try {
+        fs.writeFileSync('data.json', JSON.stringify(
+          dataArray.map(item => Object.assign({}, item, {
+            details: data.scheduleItems.agenda_all.agenda_courses[item.course]
+          }))
+        ))
+        res.send('💃💃💃💃💃💃💃')
+      } catch (err) {
+        res.send({ message: err.message })
+      }
     })
+    .catch((err) => {
+      console.log('ERROR!!')
+      res.status(200)
+      res.send({
+        message: err.message
+      })
+    })
+})
+
+app.get('/get-agenda-data/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, './data.json'))
 })
 
 app.listen(3000, () => {
