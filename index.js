4fb0d4fa09dc3bf7986b3bfd17c42db8f5bc8746
Nicolás A. Rodríguez <nrq5510@gmail.com>
2018-12-19 16:40:23 -0500

    Update Listen method

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ app.get('/get-agenda-data/', (req, res, next) => {
   res.sendFile(path.join(__dirname, './data.json'))
 })
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Express is working on port " + server.address().port);
-})
+var server = app.listen(process.env.PORT || 3000, () => {
+  var port = server.address().port;
+  console.log("Express is working on port " + port);
+});
